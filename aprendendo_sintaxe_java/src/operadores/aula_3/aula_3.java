c6f33c3f289c1be4429aa6d9589d8891cf1aff23
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-10-04 20:37:34 -0300

    Curso java DIO

---
+++ aprendendo_sintaxe_java/src/operadores/aula_3/aula_3.java
@@ -0,0 +1,38 @@
+package operadores.aula_3;
+
+import java.util.Date;
+
+public class aula_3 {
+    public static void main(String[] args) {
+
+//        Atribuição
+
+//        Representado pelo símbolo de igualdade "=".
+//        O operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável. em Java, definimos um tipo, nome e opcionalmente atribuímos um valor à variável através do operador de atribuição. Exemplos abaixo:
+
+        //classe Operadores.java
+
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+
+
+//        Aritméticos
+
+//        O operador aritmético, é utilizado para realizar operações matemáticas entre valores numéricos, podendo se tornar ou não uma expressão mais complexa.
+//        Os operadores aritméticos são: + (adição), - (subtração), * (multiplicação) e / (divisão).
+
+        //classe Operadores.java
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+    }
+}
